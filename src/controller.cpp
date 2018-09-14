e9acf3f93617b246371a0797bbfc8dfcc96679a6
Lawrence Kesteloot <lk@teamten.com>
2018-09-14 14:54:25 -0700

    Pass protobuf messages around.

--- src/controller.cpp
+++ src/controller.cpp
@@ -6,6 +6,7 @@
 #include <nng/protocol/reqrep0/req.h>
 
 #include "controller.hpp"
+#include "Drp.pb.h"
 
 static void fatal(const char *func, int rv)
 {
@@ -16,11 +17,9 @@ static void fatal(const char *func, int rv)
 int start_controller(const Parameters &parameters) {
     nng_socket sock;
     int rv;
-    size_t sz;
     char *buf = NULL;
-    uint8_t cmd[6];
-
-    strcpy((char *) cmd, "hello");
+    Drp::Request request;
+    Drp::Response response;
 
     if ((rv = nng_req0_open(&sock)) != 0) {
         fatal("nng_socket", rv);
@@ -28,17 +27,30 @@ int start_controller(const Parameters &parameters) {
     if ((rv = nng_listen(sock, "tcp://127.0.0.1:1120", NULL, 0)) != 0) {
         fatal("nng_listen", rv);
     }
-    if ((rv = nng_send(sock, cmd, sizeof(cmd), 0)) != 0) {
+
+    request.set_request_type(Drp::WELCOME);
+
+    size_t size = request.ByteSize();
+    buf = (char *) nng_alloc(size);
+    request.SerializeToArray(buf, size);
+    if ((rv = nng_send(sock, &request, sizeof(request), 0)) != 0) {
         fatal("nng_send", rv);
     }
-    if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
+    nng_free(buf, size);
+
+    if ((rv = nng_recv(sock, &buf, &size, NNG_FLAG_ALLOC)) != 0) {
         fatal("nng_recv", rv);
     }
+    response.ParseFromArray(buf, size);
+    nng_free(buf, size);
 
-    printf("Reply: <%s>\n", buf);
+    if (request.request_type() == Drp::WELCOME) {
+        std::cout << "hostname: " << response.welcome_response().hostname() <<
+            ", cores: " << response.welcome_response().core_count() << "\n";
+    } else {
+        std::cout << "Got unknown response type " << request.request_type() << "\n";
+    }
 
-    // XXX This assumes that buf is ASCIIZ (zero terminated).
-    nng_free(buf, sz);
     nng_close(sock);
 
     return 0;
--- src/worker.cpp
+++ src/worker.cpp
@@ -1,9 +1,13 @@
 
+#include <thread>
+#include <unistd.h>
+
 #include <nng/nng.h>
 #include <nng/protocol/reqrep0/rep.h>
 #include <nng/protocol/reqrep0/req.h>
 
 #include "worker.hpp"
+#include "Drp.pb.h"
 
 static void fatal(const char *func, int rv)
 {
@@ -14,6 +18,8 @@ static void fatal(const char *func, int rv)
 int start_worker(const Parameters &parameters) {
     nng_socket sock;
     int rv;
+    Drp::Request request;
+    Drp::Response response;
 
     if ((rv = nng_rep0_open(&sock)) != 0) {
         fatal("nng_rep0_open", rv);
@@ -23,21 +29,32 @@ int start_worker(const Parameters &parameters) {
     }
     for (;;) {
         char *buf = NULL;
-        size_t sz;
-        uint64_t val;
-        if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
+        size_t size;
+        if ((rv = nng_recv(sock, &buf, &size, NNG_FLAG_ALLOC)) != 0) {
             fatal("nng_recv", rv);
         }
-        printf("Received <%s>\n", buf);
+        request.ParseFromArray(buf, size);
+        nng_free(buf, size);
 
-        strcpy(buf, "world");
-        rv = nng_send(sock, buf, sz, NNG_FLAG_ALLOC);
+        std::cout << "Received " << request.request_type() << "\n";
+
+        response.set_request_type(request.request_type());
+        Drp::WelcomeResponse *welcome_response = response.mutable_welcome_response();
+        char hostname[128];
+        rv = gethostname(hostname, sizeof(hostname));
+        if (rv == -1) {
+            strcpy(hostname, "unknown");
+        }
+        welcome_response->set_hostname(hostname);
+        welcome_response->set_core_count(std::thread::hardware_concurrency());
+        size = response.ByteSize();
+        buf = (char *) nng_alloc(size);
+        response.SerializeToArray(buf, size);
+
+        rv = nng_send(sock, buf, size, NNG_FLAG_ALLOC);
         if (rv != 0) {
             fatal("nng_send", rv);
         }
-
-        // Unrecognized command, so toss the buffer.
-        // XXX nng_free(buf, sz);
     }
 
     return 0;
