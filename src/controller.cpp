11dde63ea1e0ed07b99a5355859fe32b5275190f
Lawrence Kesteloot <lk@teamten.com>
2018-09-14 20:43:42 -0700

    Implement execute command.

--- src/controller.cpp
+++ src/controller.cpp
@@ -18,8 +18,6 @@ static void fatal(const char *func, int rv)
 int start_controller(const Parameters &parameters) {
     nng_socket sock;
     int rv;
-    Drp::Request request;
-    Drp::Response response;
 
     if ((rv = nng_req0_open(&sock)) != 0) {
         fatal("nng_socket", rv);
@@ -28,22 +26,56 @@ int start_controller(const Parameters &parameters) {
         fatal("nng_listen", rv);
     }
 
-    request.set_request_type(Drp::WELCOME);
-    rv = send_message(sock, request);
-    if (rv != 0) {
-        fatal("send_message", rv);
+    // Send welcome message.
+    {
+        Drp::Request request;
+        Drp::Response response;
+
+        request.set_request_type(Drp::WELCOME);
+        rv = send_message(sock, request);
+        if (rv != 0) {
+            fatal("send_message", rv);
+        }
+
+        rv = receive_message(sock, response);
+        if (rv != 0) {
+            fatal("receive_message", rv);
+        }
+
+        if (request.request_type() == Drp::WELCOME) {
+            std::cout << "hostname: " << response.welcome_response().hostname() <<
+                ", cores: " << response.welcome_response().core_count() << "\n";
+        } else {
+            std::cout << "Got unknown response type " << request.request_type() << "\n";
+        }
     }
 
-    rv = receive_message(sock, response);
-    if (rv != 0) {
-        fatal("receive_message", rv);
-    }
+    // Send execute program message.
+    {
+        Drp::Request request;
+        Drp::Response response;
 
-    if (request.request_type() == Drp::WELCOME) {
-        std::cout << "hostname: " << response.welcome_response().hostname() <<
-            ", cores: " << response.welcome_response().core_count() << "\n";
-    } else {
-        std::cout << "Got unknown response type " << request.request_type() << "\n";
+        request.set_request_type(Drp::EXECUTE);
+        Drp::ExecuteRequest *execute_request = request.mutable_execute_request();
+        execute_request->set_executable(parameters.m_executable);
+        for (std::string argument : parameters.m_arguments) {
+            execute_request->add_argument(argument);
+        }
+        rv = send_message(sock, request);
+        if (rv != 0) {
+            fatal("send_message", rv);
+        }
+
+        rv = receive_message(sock, response);
+        if (rv != 0) {
+            fatal("receive_message", rv);
+        }
+
+        if (request.request_type() == Drp::EXECUTE) {
+            std::cout << "status: " << response.execute_response().status() << "\n";
+        } else {
+            std::cout << "Got unknown response type " << request.request_type() << "\n";
+        }
     }
 
     nng_close(sock);
--- src/worker.cpp
+++ src/worker.cpp
@@ -1,6 +1,8 @@
 
 #include <thread>
 #include <unistd.h>
+#include <cstring>
+#include <errno.h>
 
 #include <nng/nng.h>
 #include <nng/protocol/reqrep0/rep.h>
@@ -16,11 +18,66 @@ static void fatal(const char *func, int rv)
     exit(1);
 }
 
+static void handle_welcome(const Drp::WelcomeRequest &request, Drp::WelcomeResponse &response) {
+    char hostname[128];
+    int rv = gethostname(hostname, sizeof(hostname));
+    if (rv == -1) {
+        strcpy(hostname, "unknown");
+    }
+    response.set_hostname(hostname);
+    response.set_core_count(std::thread::hardware_concurrency());
+}
+
+static void handle_execute(const Drp::ExecuteRequest &request, Drp::ExecuteResponse &response) {
+    std::string executable = request.executable();
+
+    if (!is_pathname_local(executable)) {
+        // Shouldn't happen, we check this on the controller.
+        std::cerr << "Asked to run non-local executable: " << executable << "\n";
+        response.set_status(-1);
+        return;
+    }
+
+    // Set up arguments.
+    int count = request.argument_size();
+    const char **args = new const char *[count + 2];
+
+    args[0] = executable.c_str();
+    for (int i = 0; i < count; i++) {
+        args[i + 1] = request.argument(i).c_str();
+    }
+    args[count + 1] = nullptr;
+
+    // Fork a child process.
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process.
+
+        // Do not search the path and don't change the environment.
+        int result = execv(args[0], (char **) args);
+        if (result == 0) {
+            // Shouldn't get here.
+            std::cerr << "Could not execute " << executable << ": No error\n";
+        } else {
+            std::cerr << "Could not execute " << executable << ": " << strerror(errno) << "\n";
+        }
+        exit(-1);
+    }
+
+    // Parent process.
+    int status;
+    wait4(pid, &status, 0, nullptr);
+
+    // Free up our arguments.
+    delete[] args;
+    args = nullptr;
+
+    response.set_status(WEXITSTATUS(status));
+}
+
 int start_worker(const Parameters &parameters) {
     nng_socket sock;
     int rv;
-    Drp::Request request;
-    Drp::Response response;
 
     if ((rv = nng_rep0_open(&sock)) != 0) {
         fatal("nng_rep0_open", rv);
@@ -29,22 +86,32 @@ int start_worker(const Parameters &parameters) {
         fatal("nng_dial", rv);
     }
     for (;;) {
+        Drp::Request request;
+        Drp::Response response;
+
         rv = receive_message(sock, request);
         if (rv != 0) {
             fatal("receive_message", rv);
         }
 
         std::cout << "Received " << request.request_type() << "\n";
-
         response.set_request_type(request.request_type());
-        Drp::WelcomeResponse *welcome_response = response.mutable_welcome_response();
-        char hostname[128];
-        rv = gethostname(hostname, sizeof(hostname));
-        if (rv == -1) {
-            strcpy(hostname, "unknown");
+
+        switch (request.request_type()) {
+            case Drp::WELCOME:
+                handle_welcome(request.welcome_request(),
+                        *response.mutable_welcome_response());
+                break;
+
+            case Drp::EXECUTE:
+                handle_execute(request.execute_request(),
+                        *response.mutable_execute_response());
+                break;
+
+            default:
+                std::cerr << "Unhandled message type " << request.request_type() << "\n";
+                break;
         }
-        welcome_response->set_hostname(hostname);
-        welcome_response->set_core_count(std::thread::hardware_concurrency());
 
         rv = send_message(sock, response);
         if (rv != 0) {
