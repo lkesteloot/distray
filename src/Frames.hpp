4a01f9b3577575f8e60dbd0698d34e807a4a397f
Lawrence Kesteloot <lk@teamten.com>
2018-09-14 23:04:47 -0700

    Loop over frames.

--- src/Frames.hpp
+++ src/Frames.hpp
@@ -11,6 +11,11 @@ public:
     // Format is "first[,last[,step]]". Returns whether successful. If not,
     // also prints error to standard error.
     bool parse(const std::string &spec);
+
+    // Whether this frame is past the end (taking step into account).
+    bool is_done(int frame) const {
+        return m_step > 0 ? frame > m_last : frame < m_last;
+    }
 };
 
 #endif // FRAMES_HPP
--- src/controller.cpp
+++ src/controller.cpp
@@ -16,9 +16,94 @@ static void fatal(const char *func, int rv)
     exit(1);
 }
 
+// Returns whether successful.
+static bool copy_in(nng_socket sock, const Parameters &parameters, int frame) {
+    for (const FileCopy &fileCopy : parameters.m_in_copies) {
+        if ((frame >= 0) == fileCopy.m_either_has_parameter) {
+            Drp::Request request;
+            Drp::Response response;
+
+            request.set_request_type(Drp::COPY_IN);
+            Drp::CopyInRequest *copy_in_request = request.mutable_copy_in_request();
+            // XXX expand %d and %0Nd.
+            copy_in_request->set_pathname(fileCopy.m_destination);
+            try {
+                // XXX expand %d and %0Nd.
+                copy_in_request->set_content(read_file(fileCopy.m_source));
+            } catch (std::runtime_error e) {
+                std::cerr << "Error reading file " << fileCopy.m_source << "\n";
+                return -1;
+            }
+            int rv = send_message(sock, request);
+            if (rv != 0) {
+                fatal("send_message", rv);
+            }
+
+            rv = receive_message(sock, response);
+            if (rv != 0) {
+                fatal("receive_message", rv);
+            }
+
+            if (request.request_type() == Drp::COPY_IN) {
+                std::cout << "copy in success: " << response.copy_in_response().success() << "\n";
+                if (!response.copy_in_response().success()) {
+                    return false;
+                }
+            } else {
+                std::cout << "Got unknown response type " << request.request_type() << "\n";
+            }
+        }
+    }
+
+    return true;
+}
+
+// Returns whether successful.
+static bool copy_out(nng_socket sock, const Parameters &parameters, int frame) {
+    for (const FileCopy &fileCopy : parameters.m_out_copies) {
+        if ((frame >= 0) == fileCopy.m_either_has_parameter) {
+            Drp::Request request;
+            Drp::Response response;
+
+            request.set_request_type(Drp::COPY_OUT);
+            Drp::CopyOutRequest *copy_out_request = request.mutable_copy_out_request();
+            // XXX expand %d and %0Nd.
+            copy_out_request->set_pathname(fileCopy.m_source);
+            int rv = send_message(sock, request);
+            if (rv != 0) {
+                fatal("send_message", rv);
+            }
+
+            rv = receive_message(sock, response);
+            if (rv != 0) {
+                fatal("receive_message", rv);
+            }
+
+            if (request.request_type() == Drp::COPY_OUT) {
+                std::cout << "copy out success: " << response.copy_out_response().success() << "\n";
+                if (!response.copy_out_response().success()) {
+                    return false;
+                }
+
+                // XXX expand %d and %0Nd.
+                bool success = write_file(fileCopy.m_destination, response.copy_out_response().content());
+                if (!success) {
+                    std::cout << "Could not write local file " << fileCopy.m_destination << "\n";
+                    return false;
+                }
+            } else {
+                std::cout << "Got unknown response type " << request.request_type() << "\n";
+            }
+        }
+    }
+
+    return true;
+}
+
 int start_controller(const Parameters &parameters) {
     nng_socket sock;
     int rv;
+    const Frames &frames = parameters.m_frames;
 
     if ((rv = nng_req0_open(&sock)) != 0) {
         fatal("nng_socket", rv);
@@ -52,100 +137,58 @@ int start_controller(const Parameters &parameters) {
     }
 
     // Copy files to worker.
-    for (const FileCopy &fileCopy : parameters.m_in_copies) {
-        Drp::Request request;
-        Drp::Response response;
+    // XXX Fail everything if this fails.
+    copy_in(sock, parameters, -1);
 
-        request.set_request_type(Drp::COPY_IN);
-        Drp::CopyInRequest *copy_in_request = request.mutable_copy_in_request();
-        // XXX expand %d and %0Nd.
-        copy_in_request->set_pathname(fileCopy.m_destination);
-        try {
-            // XXX expand %d and %0Nd.
-            copy_in_request->set_content(read_file(fileCopy.m_source));
-        } catch (std::runtime_error e) {
-            std::cerr << "Error reading file " << fileCopy.m_source << "\n";
-            return -1;
-        }
-        rv = send_message(sock, request);
-        if (rv != 0) {
-            fatal("send_message", rv);
-        }
+    int frame = frames.m_first;
 
-        rv = receive_message(sock, response);
-        if (rv != 0) {
-            fatal("receive_message", rv);
-        }
+    while (!frames.is_done(frame)) {
+        std::cout << "Frame " << frame << "\n";
 
-        if (request.request_type() == Drp::COPY_IN) {
-            // XXX fail completely if this fails.
-            std::cout << "copy in success: " << response.copy_in_response().success() << "\n";
-        } else {
-            std::cout << "Got unknown response type " << request.request_type() << "\n";
-        }
-    }
+        // Copy files to worker.
+        // XXX Fail everything if this fails.
+        copy_in(sock, parameters, frame);
 
-    // Send execute program message.
-    {
-        Drp::Request request;
-        Drp::Response response;
+        // Send execute program message.
+        {
+            Drp::Request request;
+            Drp::Response response;
 
-        request.set_request_type(Drp::EXECUTE);
-        Drp::ExecuteRequest *execute_request = request.mutable_execute_request();
-        execute_request->set_executable(parameters.m_executable);
-        for (std::string argument : parameters.m_arguments) {
-            // XXX expand %d and %0Nd.
-            execute_request->add_argument(argument);
-        }
-        rv = send_message(sock, request);
-        if (rv != 0) {
-            fatal("send_message", rv);
+            request.set_request_type(Drp::EXECUTE);
+            Drp::ExecuteRequest *execute_request = request.mutable_execute_request();
+            execute_request->set_executable(parameters.m_executable);
+            for (std::string argument : parameters.m_arguments) {
+                // XXX expand %d and %0Nd.
+                execute_request->add_argument(argument);
+            }
+            rv = send_message(sock, request);
+            if (rv != 0) {
+                fatal("send_message", rv);
+            }
+
+            rv = receive_message(sock, response);
+            if (rv != 0) {
+                fatal("receive_message", rv);
+            }
+
+            if (request.request_type() == Drp::EXECUTE) {
+                std::cout << "status: " << response.execute_response().status() << "\n";
+            } else {
+                std::cout << "Got unknown response type " << request.request_type() << "\n";
+            }
         }
 
-        rv = receive_message(sock, response);
-        if (rv != 0) {
-            fatal("receive_message", rv);
-        }
+        // Copy files from worker.
+        // XXX Fail everything if this fails.
+        copy_out(sock, parameters, frame);
 
-        if (request.request_type() == Drp::EXECUTE) {
-            std::cout << "status: " << response.execute_response().status() << "\n";
-        } else {
-            std::cout << "Got unknown response type " << request.request_type() << "\n";
-        }
+        // Next frame.
+        frame += frames.m_step;
     }
 
     // Copy files from worker.
-    for (const FileCopy &fileCopy : parameters.m_out_copies) {
-        Drp::Request request;
-        Drp::Response response;
-
-        request.set_request_type(Drp::COPY_OUT);
-        Drp::CopyOutRequest *copy_out_request = request.mutable_copy_out_request();
-        // XXX expand %d and %0Nd.
-        copy_out_request->set_pathname(fileCopy.m_source);
-        rv = send_message(sock, request);
-        if (rv != 0) {
-            fatal("send_message", rv);
-        }
-
-        rv = receive_message(sock, response);
-        if (rv != 0) {
-            fatal("receive_message", rv);
-        }
-
-        if (request.request_type() == Drp::COPY_OUT) {
-            // XXX fail completely if this fails.
-            std::cout << "copy out success: " << response.copy_out_response().success() << "\n";
-            // XXX expand %d and %0Nd.
-            bool success = write_file(fileCopy.m_destination, response.copy_out_response().content());
-            if (!success) {
-                std::cout << "Could not write local file " << fileCopy.m_destination << "\n";
-                // XXX fail.
-            }
-        } else {
-            std::cout << "Got unknown response type " << request.request_type() << "\n";
-        }
-    }
+    // XXX Fail everything if this fails.
+    copy_out(sock, parameters, -1);
 
     nng_close(sock);
 
